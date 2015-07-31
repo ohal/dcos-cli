3b4b34753feaa8ce85a5faac45d0753dcf1167d1
neeral <neeral@users.noreply.github.com>
2015-07-31 12:00:33 -0700

    adding collect-logs.py , it does not get services logs yet

---
+++ cli/dcoscli/getsupport/collect-logs.py
@@ -0,0 +1,54 @@
+from subprocess import check_output
+from subprocess import call
+import json
+import time
+import datetime
+import os
+import tarfile
+
+stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
+directory = '/tmp/dreamathon/logs/' + stamp
+
+def createLogFileName(name):
+	return directory + "/" + name + ".log"
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    print("creating output directory " + directory)
+
+# master and slave logs
+with open(createLogFileName("master"), "w") as outfile:
+	print("getting logs from master")
+	call(["dcos", "node", "log", "--lines=10000", "--master"], stdout=outfile)
+
+a = check_output(["dcos", "node", "--json"])
+b = json.loads(a)
+
+for i in range(0,len(b)):
+	slaveId = b[i][u'id']
+	print("getting logs from slaveId " + slaveId)
+	with open(createLogFileName("slave_" + slaveId), "w") as outfile:
+		call(["dcos", "node", "log", "--lines=10000", "--slave=" + slaveId], stdout=outfile)
+
+# task logs
+a = check_output(["dcos", "task", "--json"])
+b = json.loads(a)
+
+for i in range(0,len(b)):
+	taskId = b[i][u'id']
+	print("getting logs from task " + taskId)
+	with open(createLogFileName("task_" + taskId), "w") as outfile:
+		call(["dcos", "task", "log", "--lines=10000", taskId], stdout=outfile)
+
+# service logs
+# TODO
+
+# aggregation
+
+a = make_tarfile(directory + ".tgz", directory)
+print(a)
+print("created " + directory + ".tgz of aggregated logs")
